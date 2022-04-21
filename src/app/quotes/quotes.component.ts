a0f97d9dce1562a888cb156b2b223fdb20440955
munga114 <mungai.mbugua@student.moringaschool.com>
2022-04-21 13:46:07 +0300

    edit new Quote2

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,7 +11,7 @@ export class QuotesComponent implements OnInit {
 
   quotes:Quote[]=[
     new Quote(1,'Mungai Mbugua','If you want to live a happy life, tie it to a goal not to people or things','Albert Einstein',0,0,new Date()),
-    new Quote(2,'Mungai Mbugua','Facetime before you meet up','James K.',9,0,new Date()),
+    new Quote(2,'Mungai Mbugua','All art is a kind of confession','James Baldwin',9,0,new Date()),
     new Quote(3,'Mungai Mbugua','Take a break from dating when exhausted','Elizabeth Olsen',0,0,new Date())
   ]
   addNewQuote(quotes:any){
