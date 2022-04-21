c582f7723d8d7bf81c3db2e67a1eb383540305fb
munga114 <mungai.mbugua@student.moringaschool.com>
2022-04-21 15:48:26 +0300

    create pipe

--- src/app/date-count.pipe.spec.ts
+++ src/app/date-count.pipe.spec.ts
@@ -1,6 +1,6 @@
 import { DateCountPipe } from './date-count.pipe';
 
-describe('DateCountPipe', () => {
+describe('DateCount', () => {
   it('create an instance', () => {
     const pipe = new DateCountPipe();
     expect(pipe).toBeTruthy();
